490cada68a243f0ab59f5c25906b6607a9798120
dnjunge <dnjunge@gmail.com>
2019-03-27 18:19:59 +0300

    FacePrinter

---
+++ FacePrinter/src/FacePrinter.java
@@ -0,0 +1,14 @@
+
+public class FacePrinter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("  /////");
+		System.out.println(" | o  o |");
+		System.out.println("(|  /\\  |)");
+		System.out.println(" | |__| |");
+		System.out.println("  -----");			
+	}
+
+}
