f7863d63391170c10dcb4db3aecc1be51ec5cce7
dnjunge <dnjunge@gmail.com>
2019-03-27 17:57:21 +0300

    NamePrinter

---
+++ NamePrinter/src/NamePrinter.java
@@ -0,0 +1,14 @@
+
+public class NamePrinter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("+------+");
+		System.out.println("|Danson|");
+		System.out.println("+------+");
+		
+
+	}
+
+}
