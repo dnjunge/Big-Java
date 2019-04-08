334d107c1cb8d033edab6fd5b19062f26dd4e6cb
dnjunge <dnjunge001@gmail.com>
2019-04-08 12:27:10 +0300

    push

--- DialogViewer/src/DialogViewer.java
+++ DialogViewer/src/DialogViewer.java
@@ -1,11 +1,15 @@
+import java.io.PrintStream;
+
 import javax.swing.JOptionPane;
 public class DialogViewer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		PrintStream screen = System.out;
+		
 		String name = JOptionPane.showInputDialog(null, "What is your name?");
-		System.out.println(name);
+		screen.println(name);
 		String task = JOptionPane.showInputDialog(null, "What would you like me to do?");
 		JOptionPane.showMessageDialog(null, "I'm sorry, " + name + ". I'm afraid I can't do that.");
 		System.exit(0);
